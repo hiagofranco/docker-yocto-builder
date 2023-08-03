b008181846903ab058174bd49190a55aaadf4825
Hiago De Franco <hiagofranco@gmail.com>
2023-08-03 09:58:39 -0300

    entrypoint.sh: save bash_history from last sessions
    Signed-off-by: Hiago De Franco <hiagofranco@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,16 @@ if [ -e "${FOLDER}/export" ]; then
 	source "${FOLDER}/export"
 fi
 
-tmux 
+# Copy bash history, if it exists from previous sessions
+if [ -e "${FOLDER}/.bash_history" ]; then
+	cp ${FOLDER}/.bash_history ~/
+fi
+
+# Call tmux terminal
+tmux
+
+# Save the new history from last session
+history -w
+cp ~/.bash_history $FOLDER
+
+exit 0
